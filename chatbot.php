7b9feb6d2888c5e44be78c9b9145c5b20d736852
lll-Mike-lll <37527876+lll-Mike-lll@users.noreply.github.com>
2018-07-19 09:27:28 +0700

    f

--- chatbot.php
+++ chatbot.php
@@ -1,2 +1,15 @@
 <?php
-echo "I am a bot";
+$access_token = 'sO3SrUVbf7dnaXtDakarPlCt4THAD8yEx9X954zAtjUSHC/BK42lcQIUAC64nYSON0U0fpCj90su/s2nuu0j8u31zKopE4cCGhjNLlcwYSudDMafhlXcJEjdsbQ77AoGses6Zt/rrsYMHboaS0NtrAdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
